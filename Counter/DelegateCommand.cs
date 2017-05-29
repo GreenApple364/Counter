9d6883cf19449f43a626b823382d2763c7c3bc90
Royl <unkounkounko@gmail.com>
2017-05-29 18:18:24 +0900

    プロジェクト ファイルを追加します。

---
+++ Counter/DelegateCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Counter
+{
+    class DelegateCommand : ICommand
+    {   
+
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action execute)  : this(execute,() => true){ }
+
+        public DelegateCommand(Action execute,Func<bool> canExecute){
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+    }
+}
---
+++ Counter/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Counter
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private int count;
+
+        public int Count
+        {
+            get { return this.count; }
+            set {
+                this.count = value;
+                this.OnPropertyChanged();
+            }
+
+        }
+
+        public int PlusOne(int current)
+        {
+            current++;
+            return current;
+
+        }
+
+        public int MinusOne(int current)
+        {
+            if (current != 0)
+            {
+                current--;
+            }
+            return current;
+        }
+
+        public ICommand Plus { get; private set; }
+        public ICommand Minus { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            count = 0;
+            this.Plus = new DelegateCommand(
+                () => Count = PlusOne(Count));
+            this.Minus = new DelegateCommand(
+                () => Count = MinusOne(Count));
+        }
+    }
+}
